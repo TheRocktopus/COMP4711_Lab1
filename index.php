786f43f5d6e14809a02f8d897a21cc2893eeb35a
TheRocktopus <matthewdbanman@gmail.com>
2015-01-07 10:49:19 -0800

    Added Student class. Created and printed two students on the index.php page.
    Signed-off-by: TheRocktopus <matthewdbanman@gmail.com>

--- index.php
+++ index.php
@@ -6,7 +6,41 @@
 	</head>
 	<body>
 		<?php
-			echo "Hello out there!"
+		
+		require_once("Student.php");
+		
+		$students = array();
+		
+		// add first student
+		$first = new Student();
+		$first->surname = "Doe";
+		$first->firstName = "John";
+		$first->add_email('home', 'john@doe.com');
+		$first->add_email('work', 'jdoe@mcdonalds.com');
+		$first->add_grade(65);
+		$first->add_grade(75);
+		$first->add_grade(55);
+		$students['j123'] = $first;
+		
+		// add second student
+		$second = new Student();
+		$second->surname = "Einstein";
+		$second->firstName = "Albert";
+		$second->add_email('home', 'albert@brainiacs.com');
+		$second->add_email('work1', 'a_einstein@bcit.ca');
+		$second->add_email('work2', 'albert@physics.mit.edu');
+		$second->add_grade(95);
+		$second->add_grade(80);
+		$second->add_grade(50);
+		$students['a456'] = $second;
+		
+		ksort($students);
+		
+		foreach( $students as $student )
+		{
+			echo $student->toString();
+		}
+		
 		?>
 	</body>
 </html>
